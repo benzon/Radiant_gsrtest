cf5c6c8c98eef590d2a517e0366d9907a978f5d1
BattleRattt <>
2018-10-22 22:32:44 -0500

    Cleaned Up Resource File

--- __resource.lua
+++ __resource.lua
@@ -1,4 +1,10 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
-client_script "client.lua"
-server_script "server.lua"
-server_script '@mysql-async/lib/MySQL.lua'
+
+client_scripts {
+	"client.lua"
+}
+
+server_scripts {
+	"server.lua",
+	"@mysql-async/lib/MySQL.lua"
+}
