14ae0615e605f183eaf0e78f8ad4c36a655cd3d9
Søren Eskildsen <soeren@bzn.dk>
2018-10-28 11:41:34 +0100

    Cleanup, Optimization, Localization, Config, License
    Added .editorconfig, to setup a coding standart for the repo.
    
    __resource.lua Changede the loading order, mysql-async was loaded after server script, changed to before, added locales, and config to server and client side.
    
    Created folders for client and server script, renamed both to main.lua.
    
    client\main.lua Cleaned up the code, moved variables to the top, changed 3600 to Config.GsrTimer to allow easy change of "GSR Memory", added chat help text, removed all notification events, added one single event for notifications, with vars text and type.
    
    server\main.lua Cleaned up the code, changed the way /gsr is handled, now using es:addCommand (To avoid chat saying command does not exist), added error handling if ID is missing, or player is not online. Changed onMySQLReady to MySQL.ready (required mysql-async 3.x.x), added localization.
    
    Localization added, so translations can be done with ease.
    
    Config file added, for changing settings, insted of in script config options.
    
    locales\en.lua English base translation file.
    
    gsr.sql cleaned up, INT changed from INT(250) - Invalid to INT(11) - Valid.
    
    Removed readme.txt added README.md with markdown.
    
    Added a valid license option, current "license" is not valid and can be avoided if a person wants, so GNU v3 is a better option.
    
    Code testet and is working.

--- __resource.lua
+++ __resource.lua
@@ -1,10 +1,16 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-client_scripts {
-	"client.lua"
+server_scripts {
+	'@mysql-async/lib/MySQL.lua',
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'config.lua',
+	'server/main.lua'
 }
 
-server_scripts {
-	"server.lua",
-	"@mysql-async/lib/MySQL.lua"
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'config.lua',
+	"client/main.lua"
 }
--- gsr.sql
+++ gsr.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS `gsr` (
-  `identifier` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
-  `time` int(250) NOT NULL,
+  `identifier` varchar(250) NOT NULL,
+  `time` int(11) NOT NULL,
   PRIMARY KEY (`identifier`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+);
